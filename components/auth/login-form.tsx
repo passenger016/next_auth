0ad9a1a20a4fd4ed8447a48251e597e5dc12ad96
Tarak Nath Paul <passenger.code016@gmail.com>
2024-09-04 19:28:23 +0530

    the form login form component is made

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -3,10 +3,21 @@
 import { CardWrapper } from "./card-wrapper";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Form, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../ui/input";
 
 import * as z from "zod";
 import { LoginSchema } from "@/schema";
+import { Button } from "../ui/button";
+import { FormError } from "../FormError";
+import { FormSuccess } from "../FormSuccess";
 
 // we are not exporting default here because this is just a component not a page
 export const LoginForm = () => {
@@ -18,6 +29,11 @@ export const LoginForm = () => {
     },
   });
 
+  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log("FORM SUBMITTED");
+    console.log(values);
+  };
+
   return (
     <CardWrapper
       headerLabel="Welcome Back"
@@ -25,7 +41,45 @@ export const LoginForm = () => {
       backButtonHref="/auth/register"
       showSocial
     >
-      Login Form
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder="john@example.com"
+                    type="email"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder="******" type="password" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormError message="" />
+          <FormSuccess message="" />
+          <Button type="submit" className="w-full">
+            Login
+          </Button>
+        </form>
+      </Form>
     </CardWrapper>
   );
 };
--- schema/index.tsx
+++ schema/index.tsx
@@ -1,6 +1,12 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
+  email: z.string().email(
+    /* you can add a property called `message:""` and whatever you add there will be displyed whenever an invalid input is 
+    encountered or else the default input will bs used */
+  ),
+  /* the .min() function is used to specify that a minimum of 1 character is required */
+  password: z.string().min(1,{
+    message:"Password is required"
+  }),
 });
