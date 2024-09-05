4e950d0f3c8f80f88e9368c2573b5f424d58e73c
Tarak Nath Paul <passenger.code016@gmail.com>
2024-09-05 15:08:03 +0530

    registration of new user process was completed other than the email verification and a new directory wnamed data and a new file under data named user.ts was created

--- actions/register.tsx
+++ actions/register.tsx
@@ -1,21 +1,45 @@
 "use server";
 
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 import * as z from "zod";
 import { RegisterSchema } from "@/schema";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
-  console.log(values); // will be logged in the server
+  // console.log(values); // will be logged in the server
 
   // client side validation can be easly bypassed
   // hence we will schema validate our data on both sides of the application (server and client)
 
   const validatedFields = RegisterSchema.safeParse(values);
 
-  // if validation is passed then validationFields.success will be set to `True` else `False` 
+  // if validation is passed then validationFields.success will be set to `True` else `False`
 
-  if (!validatedFields.success) 
-    return { error: "Invalid Credentials" };
+  if (!validatedFields.success) return { error: "Invalid Credentials" };
 
-  return { success: "Email Sent!" };
+  // destructuring the data from the validated fields once we are confirmed that the fields being entered are valid
+  const { email, password, name } = validatedFields.data;
+  // hashing the password using bcrypt
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  // confirming that email that has been entered is not already taken
+  const existingUser = await getUserByEmail(email);
+
+  // if user exists then prompt an error message
+  if (existingUser) return { error: "Email Already Taken" };
+
+  // else you can create an user
+  await db.user.create({
+    data: {
+      name,
+      email,
+      // make sure to use the hashedPassword in the database not the plain password
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created!" };
 };
