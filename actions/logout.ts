195d9f420b89e37d80d3502810dac6c9e97baa12
Tarak Nath Paul <passenger.code016@gmail.com>
2025-05-09 17:02:41 +0530

    fix(logout): correct export name from login to logout in logout.tsx

--- actions/logout.ts
+++ actions/logout.ts
@@ -2,7 +2,7 @@
 
 import { signOut } from "@/auth";
 
-export const login = async () => {
+export const logout = async () => {
   // use this server action for signOut if any additional logic is required before signing out the user like for clearing user data.
   await signOut();
 };
