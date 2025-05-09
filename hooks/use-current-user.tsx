a04d7e5a56d613fb861ef726db0de2b555751a4a
Tarak Nath Paul <passenger.code016@gmail.com>
2025-05-09 15:40:19 +0530

    feat(hooks): add hooks folder and custom useCurrentUser hook
    the hook is used to extract the session object using useSession and then exporting the session.data?.user so that it can be imported and referrenced directly

---
+++ hooks/use-current-user.tsx
@@ -0,0 +1,7 @@
+// we will export the user part from the session object so that we can use it directly instead of having to referrence it using user.session.user
+import { useSession } from "next-auth/react";
+
+export const useCurrentUser = () => {
+  const session = useSession();
+  return session.data?.user;
+};
