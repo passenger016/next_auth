20d03f621d812c4ec8f122d02f0802288ee61e2d
Tarak Nath Paul <passenger.code016@gmail.com>
2025-05-13 12:19:54 +0530

    chore(server): add neccessary comments

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -4,6 +4,8 @@ import { currentUser } from "@/lib/auth";
 import { UserInfo } from "../_components/user-info";
 
 /* In this page we will directly use server actions here to get the session data */
+/* currentUser is used to get the session data using server side actions by await auth() */
+/* useCurrentUser is used to get the session data using the client side hook useSession() */
 
 const ServerPage = async () => {
   // get the session data first
