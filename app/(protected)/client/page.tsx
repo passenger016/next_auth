767401039e6555710840385f6b14d5254a302ae8
Tarak Nath Paul <passenger.code016@gmail.com>
2025-05-13 12:19:25 +0530

    feat(client): add client page to show logged in user data without using server
    Closes #6

---
+++ app/(protected)/client/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { currentUser } from "@/lib/auth";
+import { UserInfo } from "../_components/user-info";
+import { useCurrentUser } from "@/hooks/use-current-user";
+
+/* In this page we will directly use client actions here to get the session data */
+/* useCurrentUser is used to get the session data using the client side hook useSession() */
+/* currentUser is used to get the session data using server side actions by await auth() */
+
+const ServerPage = () => {
+  // get the session data first
+  const user = useCurrentUser();
+
+  return <UserInfo label="Client component📱" user={user} />;
+};
+
+export default ServerPage;
