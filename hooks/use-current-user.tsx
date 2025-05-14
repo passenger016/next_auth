5217e5c4ddf1e03750a60f3954f071ac70aa15ca
Tarak Nath Paul <passenger.code016@gmail.com>
2025-05-14 15:26:09 +0530

    fix(auth): mark useCurrentUser hook as client component to support useSession

--- hooks/use-current-user.tsx
+++ hooks/use-current-user.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 // we will export the user part from the session object so that we can use it directly instead of having to referrence it using user.session.user
 import { useSession } from "next-auth/react";
 
