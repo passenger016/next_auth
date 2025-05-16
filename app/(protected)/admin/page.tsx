8bbc2ad854441f89842cb291dbcaae04cdfda50f
Tarak Nath Paul <passenger.code016@gmail.com>
2025-05-16 21:26:40 +0530

    refactor: protect admin page with RoleGate and render data conditionally based on role

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -2,11 +2,25 @@
 /* for using server side actions using currentRole we also need to add an `async` and `await` */
 /* if you are using useCurrentRole from the /hooks folder then you need to have this file as a client component and not use async and await */
 
-import currentRole from "@/lib/currentRole";
+import { RoleGate } from "@/components/auth/role-gate";
+import { FormSuccess } from "@/components/FormSuccess";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { UserRole } from "@prisma/client";
 
 const AdminPage = async () => {
-  const role = await currentRole();
-  return <div>Admin Page role:{role}</div>;
+  return (
+    <Card className="w-[600px]">
+      <CardHeader>
+        <p className="text-2xl font-semibold text-center">Admin Page🔑</p>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <RoleGate allowedRole={UserRole.ADMIN}>
+          {/* the children being passed here will only be rendered on the conditional rendering of the RoleGate if the user has the required rol or else not */}
+          <FormSuccess message="You are allowed to see this content" />
+        </RoleGate>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AdminPage;
