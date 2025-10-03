a560bce529f29f3c9c5a2b85c4ecd5a1fa2c4f1d
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-03 20:52:16 +0530

    feat(navbar): add sign out button to nav container for mobile view

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -6,12 +6,23 @@ import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
+import { signOut } from "next-auth/react";
 
 export const Navbar = () => {
   const pathName = usePathname();
   console.log(pathName);
 
   const [isActive, setIsActive] = useState(false);
+  // we can use the async and await logic inside a client component as well
+  // however the difference between a client and a server component is that the server component itself can be async and awaited
+  // whereas in a client component we need to define a function which can be async and awaited
+  // also note that the function defined inside the client component cannot be async itself as it is an event handler function
+  // so we define another async function inside the event handler function and call it
+  // this is because the event handler function expects a return type of void and if we make it async then it will return a promise which is not expected
+  // hence we define another async function inside the event handler function and call it
+  const signOutUser = async () => {
+    await signOut();
+  };
 
   return (
     <>
@@ -35,39 +46,45 @@ export const Navbar = () => {
       {/* navbar for tablet and mobile view */}
       <nav
         className={clsx(
-          "md:hidden absolute bg-white min-h-screen w-[75%] top-0 right-0 p-4 justify-between z-10",
+          "md:hidden md:h-auto absolute h-[100vh] bg-white w-[75%] top-0 right-0 p-4 justify-between z-10",
           isActive ? "flex" : "hidden"
         )}
       >
-        {/* this part contains other tabs in the navbar */}
-        <div className={clsx("flex flex-col mt-36 w-full")}>
-          <Button
-            asChild
-            variant={pathName === "/settings" ? "default" : "link"}
-            onClick={() => setIsActive(false)}
-          >
-            <Link href="/settings">Settings</Link>
-          </Button>
-          <Button
-            asChild
-            variant={pathName === "/client" ? "default" : "link"}
-            onClick={() => setIsActive(false)}
-          >
-            <Link href="/client">Client</Link>
-          </Button>
-          <Button
-            asChild
-            variant={pathName === "/server" ? "default" : "link"}
-            onClick={() => setIsActive(false)}
-          >
-            <Link href="/server">Server</Link>
-          </Button>
-          <Button
-            asChild
-            variant={pathName === "/admin" ? "default" : "link"}
-            onClick={() => setIsActive(false)}
-          >
-            <Link href="/admin">Admin</Link>
+        {/* outer container for both the mobile nav items and the logout button */}
+        <div className="flex flex-col mt-36 w-full justify-between">
+          {/* this part contains other tabs in the navbar */}
+          <div className={clsx("flex flex-col w-full")}>
+            <Button
+              asChild
+              variant={pathName === "/settings" ? "default" : "link"}
+              onClick={() => setIsActive(false)}
+            >
+              <Link href="/settings">Settings</Link>
+            </Button>
+            <Button
+              asChild
+              variant={pathName === "/client" ? "default" : "link"}
+              onClick={() => setIsActive(false)}
+            >
+              <Link href="/client">Client</Link>
+            </Button>
+            <Button
+              asChild
+              variant={pathName === "/server" ? "default" : "link"}
+              onClick={() => setIsActive(false)}
+            >
+              <Link href="/server">Server</Link>
+            </Button>
+            <Button
+              asChild
+              variant={pathName === "/admin" ? "default" : "link"}
+              onClick={() => setIsActive(false)}
+            >
+              <Link href="/admin">Admin</Link>
+            </Button>
+          </div>
+          <Button variant="link" onClick={signOutUser}>
+            Sign Out
           </Button>
         </div>
       </nav>
