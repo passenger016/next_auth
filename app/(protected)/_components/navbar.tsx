a1842db06279109844035f19f6c89040d9e15b57
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-03 20:20:08 +0530

    feat(navbar): add responsiveness to desktop navbar, add mobile navbar and hamburger menu button
    - Make the desktop navbar responsive using Tailwind CSS classes.
    - Add CSS "relative" position property to /app/page.tsx container div.
    - To ensure the mobile navbar is positioned correctly.
    - Add <nav> container for the mobile navbar items and styled with CSS.
    - Add hamburger menu button to toggle mobile navbar visibility.

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -2,41 +2,107 @@
 
 import { UserButton } from "@/components/auth/user-button";
 import { Button } from "@/components/ui/button";
+import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export const Navbar = () => {
   const pathName = usePathname();
   console.log(pathName);
 
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <nav className="bg-secondary flex justify-between items-center rounded-xl p-4 w-[600px]">
-      {/* this part contains other tabs in the navbar */}
-      <div className="flex g-x-2">
-        <Button
-          asChild
-          variant={pathName === "/settings" ? "default" : "outline"}
-        >
-          <Link href="/settings">Settings</Link>
-        </Button>
-        <Button
-          asChild
-          variant={pathName === "/client" ? "default" : "outline"}
-        >
-          <Link href="/client">Client</Link>
-        </Button>
-        <Button
-          asChild
-          variant={pathName === "/server" ? "default" : "outline"}
-        >
-          <Link href="/server">Server</Link>
-        </Button>
-        <Button asChild variant={pathName === "/admin" ? "default" : "outline"}>
-          <Link href="/admin">Admin</Link>
-        </Button>
-      </div>
-      {/* this part contains the user button which has a dropdown to logout of the app */}
-      <UserButton />
-    </nav>
+    <>
+      {/* hamburger menu button */}
+      <button
+        className={clsx(
+          "md:hidden hamburger hamburger--slider visible absolute top-6 right-6 z-[100]",
+          isActive ? "is-active" : ""
+        )}
+        type="button"
+        // setIsActive takes the previous state and toggles it
+        // it is not the event object rather the state stored in the isActive and it is a boolean value
+        // 'prev' here refers to the previous state of 'isActive'
+        onClick={() => setIsActive((prev) => !prev)}
+      >
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </button>
+
+      {/* navbar for tablet and mobile view */}
+      <nav
+        className={clsx(
+          "md:hidden absolute bg-white min-h-screen w-[75%] top-0 right-0 p-4 justify-between z-10",
+          isActive ? "flex" : "hidden"
+        )}
+      >
+        {/* this part contains other tabs in the navbar */}
+        <div className={clsx("flex flex-col mt-36 w-full")}>
+          <Button
+            asChild
+            variant={pathName === "/settings" ? "default" : "link"}
+            onClick={() => setIsActive(false)}
+          >
+            <Link href="/settings">Settings</Link>
+          </Button>
+          <Button
+            asChild
+            variant={pathName === "/client" ? "default" : "link"}
+            onClick={() => setIsActive(false)}
+          >
+            <Link href="/client">Client</Link>
+          </Button>
+          <Button
+            asChild
+            variant={pathName === "/server" ? "default" : "link"}
+            onClick={() => setIsActive(false)}
+          >
+            <Link href="/server">Server</Link>
+          </Button>
+          <Button
+            asChild
+            variant={pathName === "/admin" ? "default" : "link"}
+            onClick={() => setIsActive(false)}
+          >
+            <Link href="/admin">Admin</Link>
+          </Button>
+        </div>
+      </nav>
+      {/* navbar for screens larger than medium view */}
+      <nav className="bg-secondary hidden md:flex justify-between items-center rounded-xl p-4 md:w-[600px]">
+        {/* this part contains other tabs in the navbar */}
+        <div className="flex gap-x-2">
+          <Button
+            asChild
+            variant={pathName === "/settings" ? "default" : "outline"}
+          >
+            <Link href="/settings">Settings</Link>
+          </Button>
+          <Button
+            asChild
+            variant={pathName === "/client" ? "default" : "outline"}
+          >
+            <Link href="/client">Client</Link>
+          </Button>
+          <Button
+            asChild
+            variant={pathName === "/server" ? "default" : "outline"}
+          >
+            <Link href="/server">Server</Link>
+          </Button>
+          <Button
+            asChild
+            variant={pathName === "/admin" ? "default" : "outline"}
+          >
+            <Link href="/admin">Admin</Link>
+          </Button>
+        </div>
+        {/* this part contains the user button which has a dropdown to logout of the app */}
+        <UserButton />
+      </nav>
+    </>
   );
 };
