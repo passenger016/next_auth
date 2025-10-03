c73a60991b37ad7306d93861fe0103827ab86a04
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-03 20:55:25 +0530

    feat(form): add responsiveness logic to the login, signup and other forms
    - Updated CardWrapprer component to be responsive on smaller screens

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -26,7 +26,7 @@ export const CardWrapper = ({
   showSocial,
 }: CardWrapperProps) => {
   return (
-    <Card className="w-[400px] shadow-md">
+    <Card className="w-[90%] md:w-[400px] shadow-md">
       <CardHeader>
         <Header label={headerLabel} />
       </CardHeader>
