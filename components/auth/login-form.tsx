df97a5ff5f15cc9beb7b12f8653efa22d72a5e9a
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-04 20:13:17 +0530

    feat(login-form): add password visibility toggle for password input

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { LuEye } from "react-icons/lu";
+import { LuEyeClosed } from "react-icons/lu";
 import { CardWrapper } from "./card-wrapper";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -37,6 +39,7 @@ export const LoginForm = () => {
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
   const [showTwoFactor, setShowTwoFactor] = useState(false);
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
 
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
@@ -114,12 +117,28 @@ export const LoginForm = () => {
                   <FormItem>
                     <FormLabel>Password</FormLabel>
                     <FormControl>
-                      <Input
-                        {...field}
-                        placeholder="******"
-                        type="password"
-                        disabled={isPending}
-                      />
+                      <div className="relative">
+                        {/* the password input is controlled by React Hook Form using the 'field' object, which provides value and onChange. 
+                            To toggle password visibility, change the type prop between "password" and "text" based on isPasswordVisible. No separate state for the value is needed. */}
+                        <Input
+                          {...field}
+                          placeholder="******"
+                          type={isPasswordVisible ? "text" : "password"}
+                          disabled={isPending}
+                          className="relative"
+                        />
+                        {/* top-1/2: moves the button to 50% from the top of the container.
+                        -translate-y-1/2: shifts it up by half its own height, perfectly centering it vertically. */}
+                        {/* IMPORTANT: By default, a <button> inside a form will act as type="submit" if no type is specified. 
+                        This means clicking it will submit the form, which is not what you want for actions like toggling password visibility. */}
+                        <button
+                          type="button"
+                          className="absolute right-0 top-1/2 -translate-y-1/2 p-2" onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                        >
+                          {/* or you can use 'prev' state like  onClick={()=>setIsPasswordVisible((prev) => !prev)} */}
+                          {isPasswordVisible ? <LuEye /> : <LuEyeClosed />}
+                        </button>
+                      </div>
                     </FormControl>
                     {/* the asChild prop tells the Button to render Link component as the actual DOM component and not as a <button> while still applying all the styles of button */}
                     <Button
