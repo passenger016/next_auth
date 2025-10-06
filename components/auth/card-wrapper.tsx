48ab384c3b4dde1f5abbc4ac1d5d94ecc39ecced
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-06 19:19:12 +0530

    feat(ui): add horizontal divider with centered "or"
    - for clear separation between the credential login
    - and the OAuth login options

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -7,7 +7,7 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
-import { Socials } from "@/components/auth/socials";  
+import { Socials } from "@/components/auth/socials";
 import { BackButton } from "./back-button";
 
 interface CardWrapperProps {
@@ -31,14 +31,20 @@ export const CardWrapper = ({
         <Header label={headerLabel} />
       </CardHeader>
       <CardContent>{children}</CardContent>
+      <div className="flex items-center w-full my-2 px-6">
+        {/* flex-1 is used to make sure that both the hr tags take equal space */}
+        <hr className="flex-1 border-t border-gray-300" />
+        <span className="mx-4 text-gray-500 font-semibold">or</span>
+        <hr className="flex-1 border-t border-gray-300" />
+      </div>
       {showSocial && (
         <CardFooter>
           <Socials />
         </CardFooter>
       )}
       <CardFooter>
-          <BackButton label={backButtonLabel} href={backButtonHref} />
-        </CardFooter>
+        <BackButton label={backButtonLabel} href={backButtonHref} />
+      </CardFooter>
     </Card>
   );
 };
