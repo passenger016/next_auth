b6ff2593a564d47c32364c8cb5ec718385e51ec2
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-06 14:08:25 +0530

    feat(email): add two factor code email template

--- lib/emailTemplate.ts
+++ lib/emailTemplate.ts
@@ -10,6 +10,12 @@ interface passwordResetEmailTemplateProps {
   projectName: string;
 }
 
+interface twoFactorAuthEmailTemplateProps {
+  token: string;
+  userName: string | null;
+  projectName: string;
+}
+
 // template for verification email
 export const verificationEmailTemplate = ({
   confirmLink,
@@ -67,6 +73,38 @@ export const passwordResetEmailTemplate = ({
           If the button above doesn't work, copy and paste this link into your browser:<br>
           <a href="${resetLink}" style="color: #2563eb; text-decoration: underline;">${resetLink}</a>
         </div>
+      </td>
+    </tr>
+  </table>
+</div>
+  `;
+};
+
+// template for 2FA code email
+// this email won't contain a link instead will contain a 6 digit code which is the token itself
+export const twoFactorAuthEmailTemplate = ({
+  token,
+  userName,
+  projectName,
+}: twoFactorAuthEmailTemplateProps) => {
+  return `
+    <div style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; background: #f6f8fa; padding: 0; margin: 0;">
+  <table width="100%" cellpadding="0" cellspacing="0" style="max-width: 600px; margin: 40px auto; background: #fff; border-radius: 10px; overflow: hidden; box-shadow: 0 4px 12px rgba(0,0,0,0.08);">
+    <tr>
+      <td style="background: #2563eb; text-align: center; padding: 32px 0;">
+        <span style="font-size: 2rem; font-weight: 800; color: #fff; letter-spacing: 1px;">${projectName}</span>
+      </td>
+    </tr>
+    <tr>
+      <td style="padding: 32px;">
+        <p style="font-size: 1rem; margin-bottom: 16px;">Hello <strong style="color: #2563eb;">${userName ? userName : ""}</strong>,</p>
+        <h2 style="font-size: 1.25rem; font-weight: 600; margin-bottom: 24px;">Your 2FA Code</h2>
+        <div style="display: block; width:100%;">
+          <span style="display:inline-block; color: #2563eb; font-weight: bold; font-size: 5rem; padding: 0px 16px; user-select: text; width:100%; text-align: center; margin:0; line-height: 1.2;"> ${token} </span>
+          <span style="display:inline-block; font-size: 0.95rem; color: #555; width:100%; text-align: center; width:100%; text-align: center;">Long press to copy the code on mobile.</span>
+        </div>
+        <p style="margin-top: 24px;">Please do not share this code with anyone. If you did not request this code, you can safely ignore this email.</p>
+        <p style="font-size: 0.95rem; color: #555; margin: 24px 0 8px;">For security, this token may expire after a short time.</p>
       </td>
     </tr>
   </table>
