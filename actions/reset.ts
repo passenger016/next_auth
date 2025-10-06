c70e98ce568c4d59b218b8ea100792cbfd88a273
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-06 12:47:32 +0530

    feat(reset): add send password reset email using nodemailer
    - the functionality to send password reset email was implemented with resend.
    - changed it to be sent using nodemailer.

--- actions/reset.ts
+++ actions/reset.ts
@@ -5,6 +5,7 @@ import { getUserByEmail } from "@/data/user";
 import * as z from "zod";
 import { sendPasswordResetEmail } from "@/lib/mail";
 import { generatePasswordResetToken } from "@/lib/tokens";
+import { sendPasswordResetEmailNodemailer } from "@/lib/mailUsingNodemailer";
 
 export const reset = async (values: z.infer<typeof PasswordResetSchema>) => {
   console.log(values); // will be logged in the server
@@ -23,10 +24,19 @@ export const reset = async (values: z.infer<typeof PasswordResetSchema>) => {
 
   const passwordResetToken = await generatePasswordResetToken(email);
 
-  await sendPasswordResetEmail(
-    passwordResetToken.email,
-    passwordResetToken.token
-  );
+  // sending email using nodemailer
+  await sendPasswordResetEmailNodemailer({
+    to: passwordResetToken.email,
+    subject: "Password Reset Request",
+    token: passwordResetToken.token,
+    userName: existingUser.name,
+  });
+
+  // sending email using the resend service -- commented out because we are now using nodemailer
+  // await sendPasswordResetEmail(
+  //   passwordResetToken.email,
+  //   passwordResetToken.token
+  // );
 
   // if everything went right then throw an success email
   return { success: "Password reset email sent" };
