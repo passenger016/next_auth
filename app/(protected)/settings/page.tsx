8517e2e041567fea1d7ddeb9f2b9f41b052b870b
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-18 14:07:38 +0530

    feat(settings): add update name functionality to settings page.
    - updating settings page to include a button that updates the user's name.
    - using server action to handle the name update logic.
    - using useTransition hook to manage UI state during the update process.
    
    - updates the name in the db.
    - but doesn't render the updated name on the UI immediately.
    - next I will update the session data to reflect the updated name on the UI.

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -4,8 +4,21 @@
 
 import { useCurrentUser } from "@/hooks/use-current-user";
 import { signOut } from "next-auth/react";
+import { CardContent, CardHeader, Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { settings } from "@/actions/settings";
+import { useTransition } from "react";
 
+/* 
+  NOTE: async await functions can be used in a client component but they cannot be used directly in the component body
+  they can be used in event handlers or useEffect hooks 
+*/
 const SettingsPage = () => {
+  // we are using useTransition to manage the pending state of the server action
+  // isPending and startTransition are destructured from useTransition hook
+  // they are the standard way of handling transitions in React
+  const [isPending, startTransition] = useTransition();
+
   // we are using the useCurrentUser hook to directly get the data of the `user` other than having to do a session.data?.user everytime
   // we have to stringy the data in order to display it
   const user = useCurrentUser();
@@ -14,13 +27,28 @@ const SettingsPage = () => {
     // or import logout() from @/actions/logout.ts and call logout() to implement server actions for logout logic.
     // the above approach is better if there is additional logic required before logging out the user or else the current logic works perfectly.
   };
+  const updateName = () => {
+    startTransition(() => {
+      settings({
+        name: "New Name",
+      });
+    });
+  };
 
   return (
-    <div className="bg-white rounded-xl p-10">
-      <button type="submit" onClick={signOutUser}>
-        Sign Out
-      </button>
-    </div>
+    <Card className="w-[80%] md:w-[600px] shadow-md">
+      <CardHeader>
+        <h2 className="text-2xl font-semibold text-center">⚙️Settings</h2>
+      </CardHeader>
+      <CardContent>
+        {/* Button with sign out functionality as a example of implementing signOut or related server actions logic on a client component */}
+        {/* <Button variant="outline" onClick={signOutUser}>
+          Sign Out
+        </Button> */}
+        {/* Button will be diabled during the transition */}
+        <Button onClick={updateName} disabled={isPending}>Update Name</Button>
+      </CardContent>
+    </Card>
   );
 };
 
