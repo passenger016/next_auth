1254ac4148054de20c1427ae414816d3e9f10689
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-27 13:43:17 +0530

    feat(settings): add reset button to security page
    - also shifted useEffect below the useForm declaration
    - for better logic handling

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -60,6 +60,16 @@ const SettingsPage = () => {
       isTwoFactorEnabled: user?.isTwoFactorEnabled ?? false,
     },
   });
+
+  // RHF gives a watch method to watch specific form fields
+  // we will keep an eye on both the password and confirm password fields, it will track changes realtime
+  // NOTE: using onChange() function with RHF caused unexpected behaviours hence we are using watch()
+  // so that we can disable the submit button if any of them is empty
+  const { watch, reset } = form;
+  const nameInputValue = watch("name");
+  const emailInputValue = watch("email");
+  const isTwoFactorEnabledValue = watch("isTwoFactorEnabled");
+
   // Reset form values whenever user changes
   // React Hook Form's defaultValues are only set during the initial render and do not update automatically when the user data changes asynchronously.
   // hence we use useEffect to reset the form values whenever the user data changes.
@@ -73,14 +83,6 @@ const SettingsPage = () => {
     });
   }, [user, form]);
 
-  // RHF gives a watch method to watch specific form fields
-  // we will keep an eye on both the password and confirm password fields, it will track changes realtime
-  // NOTE: using onChange() function with RHF caused unexpected behaviours hence we are using watch()
-  // so that we can disable the submit button if any of them is empty
-  const { watch } = form;
-  const nameInputValue = watch("name");
-  const emailInputValue = watch("email");
-  const isTwoFactorEnabledValue = watch("isTwoFactorEnabled");
   // we will disable the button if there are no changes made to the form
   // NOTE: contrary to password reset form, here we will disable the button if no changes are made
   // since the default values are already populated with the current user data unlike the password reset form which had empty default values
@@ -241,9 +243,19 @@ const SettingsPage = () => {
             {/* display error or success message if any */}
             <FormError message={error} />
             <FormSuccess message={success} />
-            <Button type="submit" disabled={isPending || disableButton}>
-              {isPending ? <Spinner /> : "Update"}
-            </Button>
+            <div className="flex gap-x-2">
+              <Button type="submit" disabled={isPending || disableButton}>
+                {isPending ? <Spinner /> : "Update"}
+              </Button>
+              <Button
+                variant="secondary"
+                type="reset"
+                disabled={isPending || disableButton}
+                onClick={() => reset()}
+              >
+                Cancel
+              </Button>
+            </div>
           </form>
         </Form>
       </CardContent>
