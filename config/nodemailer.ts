3e7ebb7d7fbc6c8d6a042cdea02c3f3c782163e4
Tarak Nath Paul <passenger.code016@gmail.com>
2025-10-29 17:26:43 +0530

    fix(nodemailer): moved host email for sending the mails to env

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -1,19 +1,18 @@
 // this file is for confirguing nodemailer instance
 // then we will export the transporter and the sender email address
 // so that they can be used in other parts of the application for sending emails using nodemailer
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 // constant containing the email address of the sender
-export const accountEmail = 'passenger.code016@gmail.com'
-
+export const accountEmail = process.env.EMAIL_FROM;
 
 // we will configure a transporter using GMAIL as a service
 export const transporter = nodemailer.createTransport({
-    service: 'Gmail', // ids are case-insensitive
-    auth: {
-        user: accountEmail,
-        pass: process.env.EMAIL_PASSWORD,
-    }
+  service: "Gmail", // ids are case-insensitive
+  auth: {
+    user: accountEmail,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 // a common resuable HTML template for the email body is created under /utils/email-template.js
